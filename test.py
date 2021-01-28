42f4e84424cffb736161f5fc791f81bbf5915d12
Jun Wei Huang <U0413023@gm.nuu.edu.tw>
2021-01-28 11:59:02 +0800

    20210128-1

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 # img = cv2.imread('test.jpg',cv2.IMREAD_COLOR)#彩色
 img = cv2.imread('test.jpg',1)#彩色
 # img = cv2.imread('test.jpg',0)#黑白
 # img = cv2.imread('test.jpg',cv2.IMREAD_GRAYSCALE)#黑白色
 
 print(img)#確認資訊在不在
-cv2.namedWindow('test',cv2.WINDOW_NORMAL)#可調整視窗大小
 
-cv2.imshow('test',img)#看圖
-cv2.imwrite('test1.jpg',img)#寫入儲存檔案
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2顯示圖案
+# cv2.namedWindow('test',cv2.WINDOW_NORMAL)#可調整視窗大小
+# cv2.imshow('test',img)#看圖
+# cv2.imwrite('test1.jpg',img)#寫入儲存檔案
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#matplotlib顯示圖案
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
+plt.show()
