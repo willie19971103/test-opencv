f945ec7e583ef94e164848728fac1a75590f8497
Jun Wei Huang <U0413023@gm.nuu.edu.tw>
2021-01-30 15:01:45 +0800

    20210130-3

--- test-19.py
+++ test-19.py
@@ -5,16 +5,27 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('IMG.jpg')#匯入檔案
 img1 = img.copy() #複製一份檔案
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#轉換色域
 
 blurred = cv2.GaussianBlur(imgray, (11, 11), 0) #高斯濾鏡
-edges = cv2.Canny(blurred, 60, 160) #Canny
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-print(len(contours))
-draw = cv2.drawContours(img1, contours, -1, (0, 255, 0), 5)
 
+
+edges = cv2.Canny(blurred, 150, 200) #Canny偵測邊界的MAX MIN
+
+
+
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)#找到邊界
+print(len(contours))
+draw = cv2.drawContours(img1, contours, -1, (0, 255, 0), 5)#畫出所有輪廓
+
+
+#作圖
 plt.subplot(131), plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)), plt.title('Original Image')
 plt.subplot(132), plt.imshow(edges,cmap='gray'), plt.title('Edge Image')
 plt.subplot(133), plt.imshow(cv2.cvtColor(draw,cv2.COLOR_BGR2RGB)), plt.title('contour Image')
 
 plt.show()
+
+
+
+
